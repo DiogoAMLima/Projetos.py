e4713c3975c5bb1a45245470965966e9ca5cc0de
Diogo Lima <diogoamlima@gmail.com>
2022-02-24 18:13:48 -0300

    Novos códigos

---
+++ PycharmProjects/exercicios/concurso.py
@@ -0,0 +1,8 @@
+ganhador1 = ((780000 / 100) * 46)
+print(f'Ganhador 1 receberá: {ganhador1}')
+
+ganhador2 = ((780000 / 100) * 32)
+print(f'Ganhador 2 receberá: {ganhador2}')
+
+ganhador3 = ((780000 / 100) * 22)
+print(f'Ganhador 3 receberá: {ganhador3}')
---
+++ PycharmProjects/exercicios/contagem_regres.py
@@ -0,0 +1,6 @@
+num = 10
+while num != -1:
+    print(num)
+    if num == 0:
+        print("FIM!")
+    num -= 1
