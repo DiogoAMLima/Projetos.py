ff670b58d5587370eddbd827ae265b462679e2bb
Diogo de Albuquerque <96998329+DiogoAMLima@users.noreply.github.com>
2022-02-27 14:14:36 -0300

    Delete cilindro_circular.py

--- PycharmProjects/exercicios/cilindro_circular.py
+++
@@ -1,5 +0,0 @@
-H = float(input("Informe a altura do cilindro: "))
-R = float(input("Informe o raio do cilindro: "))
-
-V = 3.141592 * (R * R) * H
-print(f'O volume do cilindro circular é: ' "{:.2f}".format(V))
