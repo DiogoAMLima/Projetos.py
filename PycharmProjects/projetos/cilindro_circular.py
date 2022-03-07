e4c9c54a8e2a3913141f8069ffde952b71abd765
Diogo de Albuquerque <96998329+DiogoAMLima@users.noreply.github.com>
2022-03-07 19:56:26 -0300

    Novo código

---
+++ PycharmProjects/projetos/cilindro_circular.py
@@ -0,0 +1,5 @@
+H = float(input("Informe a altura do cilindro: "))
+R = float(input("Informe o raio do cilindro: "))
+
+V = 3.141592 * (R * R) * H
+print(f'O volume do cilindro circular é: ' "{:.2f}".format(V))
