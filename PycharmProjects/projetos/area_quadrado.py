10d7077647936b0ba04c92bde8a05ccc5c160164
Diogo de Albuquerque <96998329+DiogoAMLima@users.noreply.github.com>
2022-03-24 23:29:33 -0300

    Novo código

---
+++ PycharmProjects/projetos/area_quadrado.py
@@ -0,0 +1,9 @@
+print('----------------ÁREA QUADRADO-----------------\n')
+L = float(input("Informe o lado do quadrado: "))
+
+areaQ = (L * L)
+print(f'A área do quadrado é: ' "{:.2f}".format(areaQ))
+
+print('\n')
+
+print('------------------------------------------------')
