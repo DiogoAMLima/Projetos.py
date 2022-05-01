6ef4c1d680c7edf8f2082ccfc5a1c74b51754625
Diogo de Albuquerque <96998329+DiogoAMLima@users.noreply.github.com>
2022-05-01 11:43:45 -0300

    Mudança no print final.

--- PycharmProjects/projetos/seno_cosseno_tangente.py
+++ PycharmProjects/projetos/seno_cosseno_tangente.py
@@ -16,5 +16,4 @@ angulo = float(input("Informe um ângulo que você deseja descobrir em seno,coss
 seno = sin(radians(angulo))
 cosseno = cos(radians(angulo))
 tangente = tan(radians(angulo))
-print('O seno, Cosseno e Tangente de {} são respectivamente: {:.2f}, {:.2f} e {:.2f}'
-      .format(angulo, seno, cosseno, tangente))
+print(f'O Seno, Cosseno e Tangente de {angulo:.1f} são respectivamente: {seno:.2f}, {cosseno:.2f} e {tangente:.2f}')
