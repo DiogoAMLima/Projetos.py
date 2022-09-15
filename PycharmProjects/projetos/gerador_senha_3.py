bc72a8bebef5c062d9929c64af31252d9f93602c
Diogo de Albuquerque <96998329+DiogoAMLima@users.noreply.github.com>
2022-09-14 22:36:00 -0300

    Update geral no código

--- PycharmProjects/projetos/gerador_senha_3.py
+++ PycharmProjects/projetos/gerador_senha_3.py
@@ -14,18 +14,18 @@ letra_maiuscula = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 juntando = num + caracteres_especiais + letra_minuscula + letra_maiuscula
 
 while True:
-    op = str(input('Para continuarmos o procedimento, pressione qualquer tecla ou s para sair... '))
+    op = str(input('\nPara continuarmos o procedimento, pressione qualquer tecla ou s para sair... '))
     if op == 's':
-        print('\033[33mFinalizando...\033[m')
+        print('\n\033[33mFinalizando...\033[m')
         break
     else:
         try:
             tamanho_da_senha = int(input('\nInforme a quantidade de caracteres: '))
-            print(f'A quantidade de caracteres escolhida foi: {tamanho_da_senha}\n')
+            print(f'A quantidade de caracteres escolhida foi: \033[34m{tamanho_da_senha}\033[m\n')
             senha = "".join(random.sample(juntando, tamanho_da_senha))
             print('\033[31mEstamos gerando sua senha, por favor aguarde...\033[m\n')
             time.sleep(4)
-            print(f'\033[34mSua senha é:\033[m {senha}')
-            print('\n')
+            print(f'\033[34mSua senha é:\033[m \033[35m{senha}\033[m')
         except (ValueError, TypeError):
             print('Tivemos um problema com os tipos de dados que você digitou! Necessário que seja um número inteiro!')
+
