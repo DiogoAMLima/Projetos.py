aa9cf1b2fa94b1ab313407886de754637ab40eb3
Diogo de Albuquerque <96998329+DiogoAMLima@users.noreply.github.com>
2022-12-12 01:35:53 -0300

    Emoji

---
+++ General/emoji_colors.py
@@ -0,0 +1,14 @@
+import emoji
+
+# Alguns Emojis:
+
+print(emoji.emojize('\033[32mHello Christmans :Christmas_tree:\033[m'))  # Christmas_tree
+
+print(emoji.emojize('\033[33mHand and Button :backhand_index_pointing_right_light_skin_tone: :OK_button: \033[m'))
+# Hand and Button
+
+print(emoji.emojize('\033[31mThe year is ending :calendar:\033[m'))  # Calendar
+
+print(emoji.emojize('\033[32mCheck :check_mark_button: and Check :check_mark:\033[m'))  # Check mark
+
+print(emoji.emojize('\033[34mCross mark :cross_mark: and Cross mark button :cross_mark_button:\033[m'))  # Cross mark
