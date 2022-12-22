21204bbec96c2718f06f21e86dbde5ac715ae37c
Diogo de Albuquerque <96998329+DiogoAMLima@users.noreply.github.com>
2022-12-22 01:23:08 -0300

    Função Recursiva Sequência de Fibonacci

---
+++ Functions/funcao_rec_seq_fibonacci.py
@@ -0,0 +1,28 @@
+import time
+
+
+def fibonacci(index):
+    if index == 1:
+        return 0
+    elif index == 2:
+        return 1
+    else:
+        return fibonacci(index - 1) + fibonacci(index - 2)
+
+
+while True:
+    try:
+        n = int(input('Deseja exibir até qual termo? '))
+        print()
+    except (ValueError, TypeError):
+        print('\033[31mTivemos um problema com os tipos de dados que você digitou! '
+              'Necessário que seja um número inteiro!\033[m')
+    for valor in range(1, n + 1):
+        print(f'\033[97m{fibonacci(valor)}\033[m')
+        time.sleep(0.5)
+    op = str(input('\nDeseja continuar? \033[34m[S/N]\033[m ')).strip().upper()
+    if op == 'N':
+        print('\n\033[33mFinalizando...\033[m')
+        break
+    else:
+        pass
