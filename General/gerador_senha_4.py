a43e4a4da0c29a815d18a1d989dd8779d0ba3fc5
Diogo de Albuquerque <96998329+DiogoAMLima@users.noreply.github.com>
2022-12-23 14:50:19 -0300

    Gerador de Senha 4

---
+++ General/gerador_senha_4.py
@@ -0,0 +1,12 @@
+import random
+import string
+import time
+
+qntd = int(input('Informe quantos caracteres terá a senha: '))
+
+password = "".join(random.choices(string.printable, k=qntd)).strip()
+
+print('\n\033[31mEstamos gerando sua senha, por favor aguarde!!!\033[m')
+time.sleep(1.5)
+
+print(f'\nSua senha é: \033[34m{password}\033[m')
