c25c8b91311168f5f9da494dbbc9bf1a42f11e35
Diogo de Albuquerque <96998329+DiogoAMLima@users.noreply.github.com>
2023-01-19 23:51:43 -0300

    Code Improved

--- Functions/função_opções.py
+++ Functions/função_opções.py
@@ -1,10 +1,10 @@
 def escolha():
     while True:
         try:
+            input('\nAperte enter para o Menu aparecer: ')
             print()
-            input('Aperte enter para o Menu aparecer: ')
-            print('\033[31m—\033[m' * 13, 'MENU', '\033[31m—\033[m' * 13)
-            print('\033[32mOPÇÃO 1 - SOMAR\033[m\n'
+            print('\033[31m—\033[m' * 13, '\033[36mMENU\033[m', '\033[31m—\033[m' * 13)
+            print('\n\033[32mOPÇÃO 1 - SOMAR\033[m\n'
                   '\033[32mOPÇÃO 2 - SUBTRAIR\033[m\n'
                   '\033[32mOPÇÃO 3 - MULTIPLICAR\033[m\n'
                   '\033[32mOPÇÃO 4 - DIVIDIR\033[m\n'
@@ -15,58 +15,62 @@ def escolha():
             op = int(input('Informe uma das opções acima ou qualquer outro número para sair: '))
             print()
         except(ValueError, TypeError):
-            print('\033[0;35mERRO: por favor, digite um número inteiro válido\033[m')
+
+            print('\033[0;35mERRO: por favor, digite um número int ou float válido\033[m')
             continue
         except KeyboardInterrupt:
             print('\n\033[35mEntrada de dados interrompida pelo usuário.\033[m')
         else:
             if op == 1:
-                x = int(input('Informe o primeiro número: '))
-                y = int(input('Informe o segundo número: '))
+                x = float(input('Informe o primeiro número: '))
+                y = float(input('Informe o segundo número: '))
                 soma = x + y
-                print(f'{x} + {y} = \033[33m{soma}\033[m')
+                print(f'\n{x} + {y} = \033[33m{soma:.2f}\033[m')
                 print()
             elif op == 2:
-                x = int(input('Informe o primeiro número: '))
-                y = int(input('Informe o segundo número: '))
+                x = float(input('Informe o primeiro número: '))
+                y = float(input('Informe o segundo número: '))
                 subtrair = x - y
-                print(f'{x} - {y} = \033[33m{subtrair}\033[m')
+                print(f'\n{x} - {y} = \033[33m{subtrair:.2f}\033[m')
                 print()
             elif op == 3:
-                x = int(input('Informe o primeiro número: '))
-                y = int(input('Informe o segundo número: '))
+                x = float(input('Informe o primeiro número: '))
+                y = float(input('Informe o segundo número: '))
                 multiplicar = x * y
-                print(f'{x} * {y} = \033[33m{multiplicar}\033[m')
+                print(f'\n{x} * {y} = \033[33m{multiplicar:.2f}\033[m')
                 print()
             elif op == 4:
                 x = float(input('Informe o primeiro número: '))
                 y = float(input('Informe o segundo número: '))
                 divisao = x / y
-                print(f'{x} / {y} = \033[33m{divisao}\033[m')
+                print(f'\n{x} / {y} = \033[33m{divisao:.2f}\033[m')
                 print()
             elif op == 5:
                 x = float(input('Informe o primeiro número: '))
                 y = float(input('Informe o segundo número: '))
                 quociente = x // y
                 resto = x % y
-                print(f'O quociente de {x} // {y} e resto de {x} % {y} são \033[33m{quociente} e {resto}\033[m')
+                print(f'\nO quociente de {x} // {y} e resto de {x} % {y} são \033[33m{quociente} e {resto}\033[m')
                 print()
             elif op == 6:
-                num = int(input('Informe um número: '))
-                f = 1
-                for c in range(num, 0, -1):
-                    f *= c
-                print(f'O fatorial é \033[33m{f}\033[m')
+                try:
+                    num = int(input('Informe um número: '))
+                    f = 1
+                    for c in range(num, 0, -1):
+                        f *= c
+                    print(f'\nO fatorial é \033[33m{f}\033[m')
+                except ValueError:
+                    print('\n\033[31mO tipo de dado digitado está incorreto...\033[m')
                 print()
             elif op == 7:
-                n = int(input('Informe um número: '))
+                n = float(input('Informe um número: '))
                 quadrado = n * n
-                print(f'{n} ao quadrado é \033[33m{quadrado}\033[m')
+                print(f'\n{n} ao quadrado é \033[33m{quadrado:.2f}\033[m')
                 print()
             elif op == 8:
-                n = int(input('Informe um número: '))
+                n = float(input('Informe um número: '))
                 cubo = n * n * n
-                print(f'{n} ao cubo é \033[33m{cubo}\033[m')
+                print(f'\n{n} ao cubo é \033[33m{cubo:.2f}\033[m')
                 print()
             else:
                 print('\033[34m—\033[m' * 28, '\033[34mAté mais\033[m', '\033[34m—\033[m' * 28)
