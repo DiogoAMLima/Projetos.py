48449dcd15ecad706e50ba851b67b14f1575365e
Diogo de Albuquerque <96998329+DiogoAMLima@users.noreply.github.com>
2023-03-01 01:21:58 -0300

    Documentation added and code improved

--- Arrays_Vectors/lista_matriz_2.py
+++ Arrays_Vectors/lista_matriz_2.py
@@ -1,7 +1,10 @@
+# Definindo as matrizes:
+
 matriz = [[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]]
 matriz2 = [[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]]
 simpar = scol = matrizv2 = 0
-# Definindo a matriz:
+
+# Definindo os valores da matriz:
 for lin in range(0, 4):
     for col in range(0, 3):
         matriz[lin][col] = int(input(f'Digite um valor para \033[33m[{lin},\033[m \033[36m{col}]:\033[m '))
@@ -9,10 +12,10 @@ for lin in range(0, 4):
 # Soma dos valores ímpares:
 for lin in range(0, 4):
     for col in range(0, 3):
-        print(f'\033[33m[{matriz[lin][col]:^5}]\033[m', end='')
+        print(f'\033[31m[{matriz[lin][col]:^5}]\033[m', end='')
         if matriz[lin][col] % 2 == 1:
             simpar += matriz[lin][col]
-    print()
+    print()  # Necessário para que seja mostrado ao/a usuário(a), os valores lado a lado em formato de matriz
 print(f'A soma dos valores ímpares é: \033[97m{simpar}\033[m')
 
 # Soma da 4 coluna:
@@ -25,6 +28,7 @@ for lin in range(0, 4):
     for col in range(0, 3):
         matrizv2 += matriz[lin][col] * 2
 print(f'A soma de todos os valores multiplicados por 2 é: \033[34m{matrizv2}\033[m', end='')
+print()
 
 
 # Multiplicando os valores de cada elemento:
@@ -37,3 +41,5 @@ mult = float(input('\nInforme um número para multiplicar os elementos da matriz
 for lin in range(0, 4):
     for col in range(0, 3):
         print(f'\033[33m[{matriz2[lin][col]*mult:.1f}]\033[m ', end='')
+    print()  # Necessário para que seja mostrado ao/a usuário(a), os valores lado a lado em formato de matriz
+
