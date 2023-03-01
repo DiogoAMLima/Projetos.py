7bbafd02602491fe305647bd0bafba3bf41fec9a
Diogo de Albuquerque <96998329+DiogoAMLima@users.noreply.github.com>
2023-03-01 20:52:34 -0300

    Documentation added

--- Class/classe_personagem.py
+++ Class/classe_personagem.py
@@ -1,6 +1,6 @@
 # Classe sem interação com o usuário:
 
-class Personagem1:
+class Personagem1:  # Definindo a classe com os atributos
     def __init__(self, nome, idade, poder, bounty):
         self.nome = nome
         self.idade = idade
@@ -8,7 +8,6 @@ class Personagem1:
         self.bounty = bounty
 
 
-personagem = Personagem1("Luffy", "19", "Zoan-Mítica", "3.000.000.000")
+personagem = Personagem1("Luffy", "19", "Zoan-Mítica", "3.000.000.000")  # Instânciando a classe
 print(f'O nome, idade, poder e recompensa do personagem é respectivamente: \033[31m'
       f'{personagem.nome}, {personagem.idade}, {personagem.poder}, R$ {personagem.bounty}\033[m')
-
