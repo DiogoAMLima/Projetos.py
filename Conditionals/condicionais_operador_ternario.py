465d9ee2afce6feeac10a19956b66f9f47462b3b
Diogo de Albuquerque <96998329+DiogoAMLima@users.noreply.github.com>
2023-03-01 21:07:44 -0300

    Documentation added

--- Conditionals/condicionais_operador_ternario.py
+++ Conditionals/condicionais_operador_ternario.py
@@ -5,5 +5,5 @@
 #
 # print(x)
 
-x = 35 if 4 > 5 else 8
+x = 35 if 4 > 5 else 8  # se 4 > 5, teremos o valor 35, como é False, teremos o valor do else = 8
 print(f'Seguindo o operador ternário acima temos o valor: \033[33m{x}\033[m')
