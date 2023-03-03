e70e7dc7ff01f5bee8050b156ebba78b4339bc49
Diogo de Albuquerque <96998329+DiogoAMLima@users.noreply.github.com>
2023-03-03 00:27:16 -0300

    Documentation added

--- Functions/funcao_bissecao.py
+++ Functions/funcao_bissecao.py
@@ -1,9 +1,10 @@
-def equacao():
+def equacao():  # Definindo a função e inicializando as listas
     posit = []
     neg = []
     posicao = []
     posicao2 = []
-    for i in range(-2, +1, 1):
+    #  Método consiste em dividir o intervalo [a, b], de forma iterativa, ao meio.
+    for i in range(-2, +1, 1):  # Laço de repetição para incremento e testes para adicionar na lista
         resp = (i ** 5) - (3 * i ** 4) + (3 * i ** 3) - (2 * i ** 2) - (5 * i) + 2
         if resp > 0:
             posicao.append(i)
@@ -12,7 +13,7 @@ def equacao():
     print('\n')
     print(neg)
     print(f'\nValore(s) onde tivemos raiz(es) positiva(s): {posicao}\n')
-    for j in range(0, 4):
+    for j in range(0, 4):  # Laço de repetição para incremento e testes para adicionar na lista
         resp1 = (j ** 5) - (3 * j ** 4) + (3 * j ** 3) - (2 * j ** 2) - (5 * j) + 2
         if resp1 > 0:
             posicao2.append(j)
@@ -29,7 +30,7 @@ equacao()
 
 def log():
     import math
-    k = (math.log(3 - 2, 10) - math.log(0.070, 10)) / math.log(2, 10)
+    k = (math.log(3 - 2, 10) - math.log(0.070, 10)) / math.log(2, 10)  # Achando o valor de K
     print(f'O valor de k é: \033[36m{k}\033[m e aproximando: \033[33m{math.ceil(k)}\033[m')
 
 
