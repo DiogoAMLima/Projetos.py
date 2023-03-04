65cdc8d4924b759fa4515c52c3d186a601e9603b
Diogo de Albuquerque <96998329+DiogoAMLima@users.noreply.github.com>
2023-03-03 21:37:54 -0300

    Documentation added

--- Functions/funcao_gerador_senha_3.py
+++ Functions/funcao_gerador_senha_3.py
@@ -1,21 +1,23 @@
 import random
 
 
-def cracker_password():
+def cracker_password():  # Definindo a função
     all_character = r"0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%&*/\?:"
-    character_list = list(all_character)
+    # Definindo os caracteres
+    character_list = list(all_character)  # Colocando em uma lista os caracteres definidos
 
     password = input("Digite sua senha: ")
 
     cracker = ''
 
     i = 0
-
+    # Looping que só para quando o cracker descobrir a senha (quando cracker for igual a password)
     while cracker != password:
-        cracker = random.choices(character_list, k=len(password))
-        print(f"\033[31m-=\033[m" * 10 + f"\033[35m{str(cracker)}\033[m" + "\033[31m-=\033[m" * 10)
+        cracker = random.choices(character_list, k=len(password))  # Definindo nosso cracker com o .choices novamente
+        print(f"\033[31m-=\033[m" * 10 + f"\033[35m{str(cracker)}\033[m" + "\033[31m-=\033[m" * 10)  # Passando para str
         i += 1
-        if cracker == list(password):
+        if cracker == list(password):  # Se a variável cracker for igual a senha informada
+            # que está em como tipo de lista, o código para e informa a que descobriu sua senha
             print("\nSua senha é: " + "".join(cracker))
             break
 
