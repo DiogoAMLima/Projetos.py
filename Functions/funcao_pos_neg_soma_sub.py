660e7f2793c873223996dbba66d95223b3ddc429
Diogo de Albuquerque <96998329+DiogoAMLima@users.noreply.github.com>
2023-03-03 22:05:45 -0300

    Documentation added

--- Functions/funcao_pos_neg_soma_sub.py
+++ Functions/funcao_pos_neg_soma_sub.py
@@ -3,14 +3,14 @@ def pos_soma(n):  # Função com parâmetro
         print(f'\nNúmero digitado \033[31m{n}\033[m é negativo...')
     else:
         s = 0
-        while n != 0:
+        while n != 0:  # Enquanto o número não for igual a 0, a soma continua e a subtração também
             s += n
             n -= 1
         return s
 
 
 valor = float(input('Informe um valor positivo: '))
-print(f'\nO resultado da soma é: \033[34m{pos_soma(valor)}\033[m ')
+print(f'\nO resultado da soma é: \033[34m{pos_soma(valor)}\033[m ')  # Colocando o valor informando na função
 
 
 def neg_sub(n):  # Função com parâmetro
@@ -18,11 +18,11 @@ def neg_sub(n):  # Função com parâmetro
         print(f'\nNúmero digitado \033[31m{n}\033[m é positivo...')
     else:
         s = 0
-        while n != 0:
+        while n != 0:  # Enquanto o número não for igual a 0, as somas continuam
             s += n
             n += 1
         return s
 
 
 valor2 = float(input('\nInforme um valor negativo: '))
-print(f'\nO resultado da subtração é: \033[35m{neg_sub(valor2)}\033[m')
+print(f'\nO resultado da subtração é: \033[35m{neg_sub(valor2)}\033[m')  # Colocando o valor informando na função
