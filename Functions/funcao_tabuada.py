f6bf2696ee06f16c9e9eddcad6cf5daed0e0a778
Diogo de Albuquerque <96998329+DiogoAMLima@users.noreply.github.com>
2023-03-03 22:57:34 -0300

    Documentation added and code improved

--- Functions/funcao_tabuada.py
+++ Functions/funcao_tabuada.py
@@ -2,11 +2,12 @@ def tabuada():
     while True:
         num = int(input("\nDigite um número para mostrar a tabuada ou um número negativo para sair: "))
         print()
-        if num < 0:
+        if num < 0:  # Se for menor que zero o valor informado, nem mostrará a tabuada
             print('\033[31mAté a próxima...\033[m')
             break
-        for cont in range(1, 11):
-            print(f'\033[32m{num}\033[m x \033[34m{cont}\033[m = \033[97m{num * cont}\033[m')
+        for cont in range(1, 11):  # Looping para termos os resultados de 1 a 10 (último valor não é incluído no for)
+            print(f'\033[32m{num}\033[m \033[31mx\033[m \033[34m{cont}\033[m = \033[97m{num * cont}\033[m')
+            # Multiplicando os valores
 
 
 tabuada()
