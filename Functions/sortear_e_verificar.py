8ed1f0a025958a290adc5edf53f5986676dc3e03
Diogo de Albuquerque <96998329+DiogoAMLima@users.noreply.github.com>
2023-03-04 00:41:24 -0300

    Documentation added

--- Functions/sortear_e_verificar.py
+++ Functions/sortear_e_verificar.py
@@ -2,22 +2,25 @@ from random import randint
 from time import sleep
 
 
-def sorteado(sorteados):
+def sorteado(sorteados):  # Inicializando a função com parâmetro
     print('Serão sorteados \033[97m10\033[m valores entre \033[37m-10\033[m e \033[30m10\033[m: ', end='')
-    for i in range(0, 10):
-        num = randint(-10, 10)
-        sorteados.append(num)
+    for i in range(0, 10):  # Alcance de 0 a 9 (O último número não entra no loop)
+        num = randint(-10, 10)  # Sorteando valores entre -10 e 10
+        sorteados.append(num)  # Adicionando na lista sorteados o número sorteado
+        # (variável valores no final do código inicializada, podendo assim adicionar os números na variável 
+        # passada como parâmetro)
         print(f'\033[35m{num}\033[m ', end='')
+        # end='' Serve para mostrar os valores na mesma linha
         sleep(0.4)
     print('  \033[36mFinalizado\033[m!!!')
 
 
-def verificar(sorteados):
+def verificar(sorteados):  # Inicializando a função com parâmetro e declarando as variáveis
     qntd_neg = 0
     qntd_pos = 0
     par = 0
     impar = 0
-    for valor in sorteados:
+    for valor in sorteados:  # Looping para cada valor na lista dos números sorteados
         if valor < 0:
             qntd_neg += 1
         if valor >= 0:
@@ -32,7 +35,7 @@ def verificar(sorteados):
     print(f'\nA quantidade de ímpares é: \033[34m{impar}\033[m')
 
 
-valores = list()
+valores = list()  # Criando uma lista
 
-sorteado(valores)
-verificar(valores)
+sorteado(valores)  # Passando a lista vazia como parâmetro
+verificar(valores)  # Passando a lista vazia como parâmetro
