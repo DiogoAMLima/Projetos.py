7a0e2b69a3f4cb3be57cff6e1472a86720104c64
Diogo de Albuquerque <96998329+DiogoAMLima@users.noreply.github.com>
2023-03-03 21:37:18 -0300

    Documentation added

--- Functions/funcao_gerador_senha_2.py
+++ Functions/funcao_gerador_senha_2.py
@@ -3,10 +3,12 @@ import string
 import time
 
 
-def gerador_senha_2():
+def gerador_senha_2():  # Definindo a função
     qntd = int(input('Informe quantos caracteres terá a senha: '))
-
+    # Usando a bilioteca string, não precisamos usar variáveis como no código de gerador de senha 1
     password = "".join(random.choices(string.printable, k=qntd)).replace(" ", ".")
+    # Unindo os caracteres com .join gerados pelo método choices de acordo com o string.printable que tem os caracteres
+    # de acordo com a variável k que determinará o tamanho da senha
 
     print('\n\033[31mEstamos gerando sua senha, por favor aguarde!!!\033[m')
     time.sleep(1.5)
@@ -15,4 +17,3 @@ def gerador_senha_2():
 
 
 gerador_senha_2()
-
