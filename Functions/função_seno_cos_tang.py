50b185f0b3d0f3bef10b58f153f743683e6d533f
Diogo de Albuquerque <96998329+DiogoAMLima@users.noreply.github.com>
2023-03-04 00:24:52 -0300

    Documentation added and code improved

--- Functions/função_seno_cos_tang.py
+++ Functions/função_seno_cos_tang.py
@@ -1,10 +1,11 @@
-def sen_cos_tan():
-    from math import radians, sin, cos, tan
+def sen_cos_tan():  # Inicializando a função
+    from math import radians, sin, cos, tan  # Métodos prontos para trigonometria
     angulo = float(input("Informe um ângulo que você deseja descobrir em seno,cosseno e tangente: "))
-    seno = sin(radians(angulo))
-    cosseno = cos(radians(angulo))
-    tangente = tan(radians(angulo))
-    print(f'O Seno, Cosseno e Tangente de {angulo:.1f} são respectivamente: {seno:.2f}, {cosseno:.2f} e {tangente:.2f}')
+    seno = sin(radians(angulo))  # Valor do ângulo
+    cosseno = cos(radians(angulo))  # Valor do ângulo
+    tangente = tan(radians(angulo))  # Valor do ângulo
+    print(f'O Seno, Cosseno e Tangente de \033[34m{angulo:.1f}\033[m são respectivamente: '
+          f'\033[35m{seno:.2f}\033[m, \033[37m{cosseno:.2f}\033[m e \033[97m{tangente:.2f}\033[m')
 
 
 sen_cos_tan()
