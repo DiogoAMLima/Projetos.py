9822e8d69c75718348aa955a50bef799ee733d48
Diogo de Albuquerque <96998329+DiogoAMLima@users.noreply.github.com>
2023-03-03 21:36:53 -0300

    Documentation added

--- Functions/funcao_gerador_senha.py
+++ Functions/funcao_gerador_senha.py
@@ -1,5 +1,5 @@
-def gerar_senha():
-    while True:
+def gerar_senha():  # Definindo a função
+    while True:  # Laço de repetição
         import random
         import time
         op = str(input('\nPara continuarmos o procedimento, pressione qualquer tecla ou s para sair... ')).strip()
@@ -7,6 +7,8 @@ def gerar_senha():
             print('\n\033[33mFinalizando...\033[m')
             break
         else:
+
+            # Definindo as variaveis de acordo com os nomes
 
             num = "0123456789"
 
@@ -16,12 +18,14 @@ def gerar_senha():
 
             letra_maiuscula = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
-            juntando = num + caracteres_especiais + letra_minuscula + letra_maiuscula
+            juntando = num + caracteres_especiais + letra_minuscula + letra_maiuscula  # Unindo as strings
 
             try:
                 tamanho_da_senha = int(input('\nInforme a quantidade de caracteres: '))
                 print(f'\nA quantidade de caracteres escolhida foi: \033[34m{tamanho_da_senha}\033[m\n')
                 senha = "".join(random.sample(juntando, tamanho_da_senha))
+                # Juntando as strings relacionadas com o método sample que de acordo com o tamanho,
+                # geram os caracteres para a senha
                 print('\033[31mEstamos gerando sua senha, por favor aguarde...\033[m\n')
                 time.sleep(4)
                 print(f'\033[34mSua senha é:\033[m \033[35m{senha}\033[m')
