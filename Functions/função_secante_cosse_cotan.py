3884a188a0449fb00934f4bbd84d6d53de8e04f7
Diogo de Albuquerque <96998329+DiogoAMLima@users.noreply.github.com>
2023-03-04 00:27:22 -0300

    Documentation added and code improved

--- Functions/função_secante_cosse_cotan.py
+++ Functions/função_secante_cosse_cotan.py
@@ -1,10 +1,10 @@
-def secan_cosse_cotan():
-    from math import radians, sin, cos, tan
-    angulo = float(input("Informe um ângulo que você deseja descobrir os valores de seno,cosseno, tangente, secante, "
+def secan_cosse_cotan():  # Inicializando a função
+    from math import radians, sin, cos, tan  # Métodos prontos para trigonometria
+    angulo = float(input("Informe um ângulo que você deseja descobrir os valores de seno, cosseno, tangente, secante, "
                          "cossecante e cotangente: "))
-    seno = sin(radians(angulo))
-    cosseno = cos(radians(angulo))
-    tangente = tan(radians(angulo))
+    seno = sin(radians(angulo))  # Valor do ângulo
+    cosseno = cos(radians(angulo))  # Valor do ângulo
+    tangente = tan(radians(angulo))  # Valor do ângulo
     print(f'O Seno, Cosseno e Tangente de \033[31m{angulo:.1f}°\033[m são respectivamente: '
           f'\033[34m{seno:.2f}\033[m, \033[34m{cosseno:.2f}\033[m e \033[34m{tangente:.2f}\033[m')
     secante = 1 / cosseno
@@ -13,8 +13,7 @@ def secan_cosse_cotan():
     print(f'O Secante, Cossecante, Cotangente de \033[31m{angulo:.1f}°\033[m são respectivamente: '
           f'\033[33m{secante:.2f}\033[m, \033[33m{cossecante:.2f}\033[m e '
           f'\033[33m{cotangente:.2f}\033[m')
-    print()
-    print('Importante lembrar que os valores estão com apenas duas casas decimais...')
+    print('\n\033[32mImportante lembrar que os valores estão com apenas duas casas decimais...\033[m')
 
 
 secan_cosse_cotan()
