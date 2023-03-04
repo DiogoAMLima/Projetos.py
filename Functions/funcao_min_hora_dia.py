854761c2f41873b9182823eed469ea3a00ad0336
Diogo de Albuquerque <96998329+DiogoAMLima@users.noreply.github.com>
2023-03-03 21:42:34 -0300

    Documentation added

--- Functions/funcao_min_hora_dia.py
+++ Functions/funcao_min_hora_dia.py
@@ -1,5 +1,5 @@
-def tempo():
-    try:
+def tempo():  # Inicializando a função
+    try:  # Tratamento de erros
         seg = float(input('Informe o valor em segundos: '))
 
         min = seg / 60
@@ -8,7 +8,7 @@ def tempo():
         print(f'\nO número digitado \033[32m{seg:.0f}\033[m corresponde a \033[34m{hora:.2f} hora(s)\033[m')
         dia = seg / (24 * 60 * 60)  # O dia possui 24 horas, 1 hora possui 60 minutos e 1 minuto possui 60 segundos
         print(f'\nO número digitado \033[32m{seg:.0f}\033[m corresponde a \033[35m{dia:.2f} dia(s)\033[m')
-
+        # Utilizando o :0f e :2f para determinas a quantidade de casas decimais
     except (KeyboardInterrupt, TypeError, ValueError):
         print('\n\033[31mTivermos um problema com o dado digitado '
               'ou o(a) usuário(a) interrompeu a entrada de dados.\033[m')
