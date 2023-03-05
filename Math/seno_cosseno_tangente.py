eb01079f4fba7c6c856e38576fef96c4a48a024a
Diogo de Albuquerque <96998329+DiogoAMLima@users.noreply.github.com>
2023-03-05 20:03:24 -0300

    Documentation added and code improved

--- Math/seno_cosseno_tangente.py
+++ Math/seno_cosseno_tangente.py
@@ -1,4 +1,4 @@
-from math import radians, sin, cos, tan
+from math import radians, sin, cos, tan  # Métodos prontos para trigonometria
 
 # Forma 1
 
@@ -13,7 +13,7 @@ from math import radians, sin, cos, tan
 # Forma 2
 
 angulo = float(input("Informe um ângulo que você deseja descobrir em seno,cosseno e tangente: "))
-seno = sin(radians(angulo))
-cosseno = cos(radians(angulo))
-tangente = tan(radians(angulo))
+seno = sin(radians(angulo))  # Valor do ângulo
+cosseno = cos(radians(angulo))  # Valor do ângulo
+tangente = tan(radians(angulo))  # Valor do ângulo
 print(f'O Seno, Cosseno e Tangente de {angulo:.1f} são respectivamente: {seno:.2f}, {cosseno:.2f} e {tangente:.2f}')
