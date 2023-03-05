6991265fd355c90b47ee7727415169cee21b2f4f
Diogo de Albuquerque <96998329+DiogoAMLima@users.noreply.github.com>
2023-03-05 16:55:07 -0300

    Documentation added and code improved

--- Math/mediaArt_desvio_coeficiente.py
+++ Math/mediaArt_desvio_coeficiente.py
@@ -5,25 +5,25 @@ ma = 0
 valores = []
 
 for i in range(9):
-    x = float(input(f'Informe um valor para o valor {i+1} lista: '))
-    valores.append(x)
+    x = float(input(f'Informe um valor para o valor {i+1}° lista: '))
+    valores.append(x)  # Adicionando o valor informado a lista valores
 
 # print(valores)
 
-for i in range(len(valores)):
+for i in range(len(valores)):  # O alcance irá até o tamanho de valores (9)
     ma += valores[i]
 
 ma = ma / len(valores)
 
-print(f'\nA média aritmética é: \033[34m{ma}\033[m')
+print(f'\nA média aritmética é: \033[34m{ma:.2f}\033[m')  # Apenas duas casas decimais
 
 desvio = 0
 
-for i in range(len(valores)):
+for i in range(len(valores)):  # O alcance irá até o tamanho de valores (9)
     desvio += ((valores[i] - ma) ** 2) / len(valores)
 
-print(f'\nO desvio padrão é: \033[97m{math.sqrt(desvio):.2f}\033[m')
+print(f'\nO desvio padrão é: \033[97m{math.sqrt(desvio):.2f}\033[m')  # math.sqrt = Retorna a raiz quadrada
 
 coef = (math.sqrt(desvio) / ma) * 100
 
-print(f'\nO coeficiente de variação é: \033[31m{coef:.1f}%\033[m')
+print(f'\nO coeficiente de variação é: \033[31m{coef:.1f}%\033[m')  # math.sqrt = Retorna a raiz quadrada
