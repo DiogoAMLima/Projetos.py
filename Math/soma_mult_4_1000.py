b2c4d6057d3b2fc7f7af42021bf2e4aabac3a99e
Diogo de Albuquerque <96998329+DiogoAMLima@users.noreply.github.com>
2023-03-05 20:30:07 -0300

    Documentation added and code improved

--- Math/soma_mult_4_1000.py
+++ Math/soma_mult_4_1000.py
@@ -1,12 +1,19 @@
-print('------------------------------SOMA MÚLTIPLOS DE 4------------------------------\n')
+print('\033[36m——————————————————————————— SOMA MÚLTIPLOS DE 4 ———————————————————————————\033[m')
 qntd_numeros = 0
 soma_numeros = 0
-for cont in range(0, 1001):
-    if cont % 4 == 0:
+m_4 = []
+for cont in range(0, 1001):  # Looping de 0 a 1000
+    if cont % 4 == 0:  # Verificando se é múltiplo de 4
         qntd_numeros += + 1
+        m_4.append(cont)
+        # Variável para receber a quantidade de números que são múltiplos de 4
         soma_numeros += cont
-print(f'A quantidade de números foi {qntd_numeros} e a soma dos mesmos é: {soma_numeros}')
+        # Soma dos múltiplos de 4
+print(f'\nA quantidade de números é: \033[33m{qntd_numeros}\033[m e a soma desses números é: '
+      f'\033[32m{soma_numeros}\033[m\n')
 
-print('\n')
-
-print('-------------------------------------------------------------------------------')
+j = 0
+for i in m_4:  # Mostrando os valores que são múltiplos de 4 
+    print(f'\nMúltiplo de 4: \033[34m{m_4[j]}\033[m')
+    j += 1
+print('\n\033[31m—\033[m' * 75)
