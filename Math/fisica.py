bf1170d16d985a0ef2ac01e27ff4c9f6437643fb
Diogo de Albuquerque <96998329+DiogoAMLima@users.noreply.github.com>
2023-03-05 16:50:57 -0300

    Documentation added and code improved

--- Math/fisica.py
+++ Math/fisica.py
@@ -2,114 +2,116 @@
 
 pergunta = 1
 
-while pergunta != 0:
+while pergunta != 0:  # Laço de repetição para enquanto for diferente de 0, o código continua
     print("1 - Velocidade média: vm = Δs/Δt\n")
     print("Δs = (s - so) || Δt = (t - to)\n")
     print("2 - Aceleração média: am = Δv/Δt \n")
-    print("Δv = (v - vo) || Δt = (t - to)\n")
-    while True:
-        try:
-            pergunta = int(input("Informe o número da fórmula que quer resolver ou digite 0 para sair: "))
-            if pergunta != int:
+    print("Δv = (v - vo) || Δt = (t - to)")
+    while True:  # Laço de repetição
+        try:  # Tratamento de erros
+            pergunta = int(input("\nInforme o número da fórmula que quer resolver ou digite 0 para sair: "))
+            if pergunta != int:  # Se o tipo digitado para a variável for diferente de inteiro, o laço acabará
                 break
-            print('Digite apenas 1, 2 ou 0')
+            print('\n\033[31mDigite apenas 1, 2 ou 0\033[m')
         except ValueError:
-            print('Digite um número válido')
+            print('\n\033[31mDigite um número válido\033[m')
     if pergunta == 0:
-        print("Até a próxima...")
+        print("\n\033[33mAté a próxima...\033[m")
     if pergunta == 1:
-        print("1 - Sobre velocidade média, você quer descobrir: vm(velocidade média), Δs(distancia) ou Δt(tempo) ?")
+        print("\n1 - Sobre velocidade média, você quer descobrir: vm(velocidade média), Δs(distancia) ou Δt(tempo) ?")
         print('Digite apenas vm, distancia ou tempo para continuar...')
         resposta: str = str(input())
         if resposta != 'vm' and 'distancia' and 'tempo':
-            print('Comando errado, tente novamente...')
+            print('\033[35mComando errado, tente novamente...\033[m')
         else:
             if resposta == 'vm':
-                s = float(input(f'Informe o valor de s '))
-                so = float(input(f'Informe o valor de so '))
-                t = float(input(f'Informe o valor de t '))
-                to = float(input(f'Informe o valor de to '))
+                s = float(input(f'\nInforme o valor de s: '))
+                so = float(input(f'Informe o valor de so: '))
+                t = float(input(f'Informe o valor de t: '))
+                to = float(input(f'Informe o valor de to: '))
                 try:
                     if s - so == 0:
-                        print('Não pode haver divisão por 0 pois, s - so resulta em 0... \n')
+                        print('\nNão pode haver divisão por 0 pois, s - so resulta em 0...\n')
                     if t - to == 0:
-                        print('Não pode haver divisão por 0 pois, t - to resulta em 0... \n')
+                        print('\nNão pode haver divisão por 0 pois, t - to resulta em 0...\n')
                 except ZeroDivisionError:
-                    pass
+                    print('\nTente novamente...\n')
                 try:
                     resultadovm = (s - so) / (t - to)
-                    print('A velocidade média é: '"{:.2f}".format(resultadovm), 'm/s \n')
+                    print('\n\033[34mA velocidade média é: '"{:.2f}".format(resultadovm), 'm/s\033[m\n')
                 except ZeroDivisionError:
-                    print('Tente novamente...\n')
+                    print('\nTente novamente...\n')
             if resposta == 'distancia':
-                t = float(input(f'Informe o valor de t '))
-                to = float(input(f'Informe o valor de to '))
-                vm = float(input(f'Informe o valor de vm '))
+                t = float(input(f'\nInforme o valor de t: '))
+                to = float(input(f'Informe o valor de to: '))
+                vm = float(input(f'Informe o valor de vm: '))
                 resultadods = vm * (t - to)
-                print('A distância é: '"{:.2f}".format(resultadods), 'm \n')
+                print('\n\033[34mA distância é: '"{:.2f}".format(resultadods), 'm\033[m\n')
             if resposta == 'tempo':
-                s = float(input(f'Informe o valor de s '))
-                so = float(input(f'Informe o valor de so '))
-                vm = float(input(f'Informe o valor de vm '))
+                s = float(input(f'\nInforme o valor de s: '))
+                so = float(input(f'Informe o valor de so: '))
+                vm = float(input(f'Informe o valor de vm: '))
                 try:
                     if s - so == 0:
-                        print('Não pode haver divisão por 0 pois, s - so resulta em 0...')
+                        print('\nNão pode haver divisão por 0 pois, s - so resulta em 0...\n')
                     if vm == 0:
-                        print('Não pode haver divisão por 0 pois, a vm é igual a 0...')
+                        print('\nNão pode haver divisão por 0 pois, a vm é igual a 0...\n')
                 except ZeroDivisionError:
-                    pass
+                    print('\nTente novamente...\n')
                 try:
                     resultadotp = (s - so) / vm
-                    print('O tempo é: '"{:.2f}".format(resultadotp), 's \n')
+                    print('\n\033[34mO tempo é: '"{:.2f}".format(resultadotp), 's\033[m\n')
                 except ZeroDivisionError:
-                    print('Tente novamente...\n')
+                    print('\nTente novamente...\n')
     if pergunta == 2:
-        print("2 - Sobre aceleração média, você quer descobrir: am(aceleração média), Δv(velocidade) ou Δt(tempo) ?")
+        print("\n2 - Sobre aceleração média, você quer descobrir: am(aceleração média), Δv(velocidade) ou Δt(tempo) ?")
         print('Digite apenas am, velocidade ou tempo para continuar...')
         resposta: str = str(input())
         if resposta != 'am' and 'velocidade' and 'tempo':
-            print('Comando errado, tente novamente...')
+            print('\n\033[31mComando errado, tente novamente...\033[m')
         else:
             if resposta == 'am':
-                v = float(input(f'Informe o valor de v '))
-                vo = float(input(f'Informe o valor de vo '))
-                t = float(input(f'Informe o valor de t '))
-                to = float(input(f'Informe o valor de to '))
+                v = float(input(f'\nInforme o valor de v: '))
+                vo = float(input(f'Informe o valor de vo: '))
+                t = float(input(f'Informe o valor de t: '))
+                to = float(input(f'Informe o valor de to: '))
                 try:
                     if v - vo == 0:
-                        print('Não pode haver divisão por 0 pois, v - vo é igual a 0...')
+                        print('\nNão pode haver divisão por 0 pois, v - vo é igual a 0...\n')
                     if t - to == 0:
-                        print('Não pode haver divisão por 0 pois, t - to é igual a 0...')
+                        print('\nNão pode haver divisão por 0 pois, t - to é igual a 0...\n')
                 except ZeroDivisionError:
-                    pass
+                    print('\nTente novamente...\n')
                 try:
                     resultadoam = (v - vo) / (t - to)
-                    print('A aceleração média é: '"{:.2f}".format(resultadoam), 'm/s2 \n')
+                    print('\n\033[35mA aceleração média é: '"{:.2f}".format(resultadoam), 'm/s2\033[m\n')
                 except ZeroDivisionError:
-                    print('Tente novamente...\n')
+                    print('\n\033[31mTente novamente...\033[m\n')
             if resposta == 'velocidade':
-                t = float(input(f'Informe o valor de t '))
-                to = float(input(f'Informe o valor de to '))
-                am = float(input(f'Informe o valor de am '))
+                t = float(input(f'\nInforme o valor de t: '))
+                to = float(input(f'\nInforme o valor de to: '))
+                am = float(input(f'\nInforme o valor de am: '))
                 resultadovm = am * (t - to)
-                print('A velocidade média é: '"{:.2f}".format(resultadovm), 'm/s \n')
+                print('\n\033[35mA velocidade média é: '"{:.2f}".format(resultadovm), 'm/s\033[m\n')
             if resposta == 'tempo':
-                v = float(input(f'Informe o valor de v '))
-                vo = float(input(f'Informe o valor de vo '))
-                am = float(input(f'Informe o valor de am '))
+                v = float(input(f'\nInforme o valor de v: '))
+                vo = float(input(f'Informe o valor de vo: '))
+                am = float(input(f'Informe o valor de am: '))
                 try:
                     if v - vo == 0:
-                        print('Não pode haver divisão por 0 pois, v - vo resulta em 0...')
+                        print('\nNão pode haver divisão por 0 pois, v - vo resulta em 0...\n')
                     if am == 0:
-                        print('Não pode haver divisão por 0 pois, am é igual a 0...')
+                        print('\nNão pode haver divisão por 0 pois, am é igual a 0...\n')
                 except ZeroDivisionError:
-                    pass
+                    print('\nTente novamente...\n')
                 try:
                     resultadotp = (v - vo) / am
-                    print('O tempo é: '"{:.2f}".format(resultadotp), 's \n')
+                    print('\n\03335mO tempo é: '"{:.2f}".format(resultadotp), 's\033[m\n')
                 except ZeroDivisionError:
-                    print('Tente novamente...\n')
-    elif pergunta >= 3:
-        print("Número incorreto, tente outra vez... \n")
-    elif pergunta <= -1:
-        print("Número incorreto, tente outra vez... \n")
+                    print('\nTente novamente...\n')
+    elif pergunta >= 3 or pergunta <= -1:
+        print("\n\033[36mNúmero incorreto, tente outra vez...\033[m\n")
+
+# O .format também é uma forma de mostrar as variáveis em um print. Normalmente, seu usa um f'{}' para mostrar o valor 
+# da variável, porém será mantido o .format como exemplo de forma de mostrar os valores também.
+# O resto do código são condicionais e tratamento de erros.
