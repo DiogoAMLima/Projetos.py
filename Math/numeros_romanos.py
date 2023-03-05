55bedb83d7b86aea9ebd235529feaa0d09764f66
Diogo de Albuquerque <96998329+DiogoAMLima@users.noreply.github.com>
2023-03-05 18:38:32 -0300

    Documentation added and code improved

--- Math/numeros_romanos.py
+++ Math/numeros_romanos.py
@@ -1,32 +1,38 @@
-print('———————————————————————————NÚEMROS ROMANOS———————————————————————————\n')
-print('Lista de números: 1, 5, 10, 20, 50, 100, 500, 1000\n')
-num_rom = [1, 5, 10, 20, 50, 100, 500, 1000]
-while True:
-    num = int(input('Informe um número que esteja na lista mostrada acima: '))
-    if num in num_rom:
+print('\033[37m——————————————————————————— NÚMEROS ROMANOS ———————————————————————————\033[m\n')
+print('Lista de números: \033[33m1, 5, 10, 20, 50, 100, 500, 1000\033[m')
+numeros = [1, 5, 10, 20, 50, 100, 500, 1000]
+numeros_rom = ['I', 'V', 'X', 'XX', 'L', 'C', 'D', 'M']  # Lista com os números romanos
+while True:  # Looping
+    num = int(input('\nInforme um número que esteja na lista mostrada acima: '))
+    if num in numeros:  # Verificando se o número escolhido está na lista, se estiver, 
+        # será mostrado o número romano correspondente de acordo com a lista de números romanos
         if num == 1:
-            print(f'O {num} em números romanos é: I\n')
+            print(f'\nO \033[32m{num}\033[m em números romanos é: \033[35m{numeros_rom[0]}\033[m\n')
         elif num == 5:
-            print(f'O {num} em números romanos é: V\n')
+            print(f'\nO \033[32m{num}\033[m em números romanos é: \033[35m{numeros_rom[1]}\033[m\n')
         elif num == 10:
-            print(f'O {num} em números romanos é: X\n')
+            print(f'\nO \033[32m{num}\033[m em números romanos é: \033[35m{numeros_rom[2]}\033[m\n')
         elif num == 20:
-            print(f'O {num} em números romanos é: XX\n')
+            print(f'\nO \033[32m{num}\033[m em números romanos é: \033[35m{numeros_rom[3]}\033[m\n')
         elif num == 50:
-            print(f'O {num} em números romanos é: L\n')
+            print(f'\nO \033[32m{num}\033[m em números romanos é: \033[35m{numeros_rom[4]}\033[m\n')
         elif num == 100:
-            print(f'O {num} em números romanos é: C\n')
+            print(f'\nO \033[32m{num}\033[m em números romanos é: \033[35m{numeros_rom[5]}\033[m\n')
         elif num == 500:
-            print(f'O {num} em números romanos é: D\n')
+            print(f'\nO \033[32m{num}\033[m em números romanos é: \033[35m{numeros_rom[6]}\033[m\n')
         elif num == 1000:
-            print(f'O {num} em números romanos é: M\n')
+            print(f'\nO \033[32m{num}\033[m em números romanos é: \033[35m{numeros_rom[7]}\033[m\n')
         while True:
             resp = str(input('Quer continuar? [S/N] ')).upper()[0]
             if resp in 'SN':
                 break
-            print('Responda apenas S ou N.')
+            print('\nResponda apenas \033[31mS ou N:\033[m\n')
         if resp == 'N':
-            print('ATÉ A PRÓXIMA')
+            print('\nATÉ A PRÓXIMA')
             break
     else:
-        print('Erro, digite um número que esteja nesta lista: 1, 5, 10, 20, 50, 100, 500, 1000\n')
+        print('\n\033[31mErro, digite um número que esteja nesta lista: 1, 5, 10, 20, 50, 100, 500, 1000\033[m')
+
+# numeros_rom[0] -> mostrando o valor da primeira posição da lista de números romanos
+# numeros_rom[1] -> mostrando o valor da segunda posição da lista de números romanos
+# E assim continua...
