91ad4c2d69f4383c25798d41eb578f6dd66207fa
Diogo de Albuquerque <96998329+DiogoAMLima@users.noreply.github.com>
2023-03-05 20:01:12 -0300

    Documentation added and code improved

--- Math/raizes.py
+++ Math/raizes.py
@@ -1,28 +1,28 @@
 import math
 
-a: float = float(input("Informe um valor para a: "))
-b: float = float(input("Informe um valor para b: "))
-c: float = float(input("Informe um valor para c: "))
+a = float(input("Informe um valor para a: "))
+b = float(input("Informe um valor para b: "))
+c = float(input("Informe um valor para c: "))
 
-delta = b**2 - 4 * a * c
+delta = b**2 - 4 * a * c  # Fórmula de delta na matemática
 
-print(f"O valor de delta é {delta}")
+print(f"\nO valor de delta é \033[31m{delta}\033[m")
 
 x1 = 0
 x2 = 0
 
 # Forma 1
 if a == 0:
-    print("O valor de a tem que ser diferente de 0")
+    print("\n\033[31mO valor de a tem que ser diferente de 0\033[m")
 elif delta < 0:
-    print("Sem raízes reais")
+    print("\n\033[31mSem raízes reais\033[m")
 else:
-    y = math.sqrt(delta)
+    y = math.sqrt(delta)  # Raiz quadrada de delta
     x1 = (-b + y) / (2 * a)
     x2 = (-b - y) / (2 * a)
 
-print('O valor de x1 é {:.2f}'.format(x1))
-print('O valor de x2 é {:.2f}'.format(x2))
+print(f'\nO valor de x1 é \033[35m{x1:.2f}\033[m')
+print(f'\nO valor de x2 é \033[36m{x2:.2f}\033[m')
 
 # Forma 2
 # if a == 0:
@@ -36,8 +36,12 @@ print('O valor de x2 é {:.2f}'.format(x2))
 # print("x1: {}, x2: {}".format(x1, x2))
 
 if delta > 0:
-    print("Delta tem duas raízes reais")
+    print("\n\033[34mDelta tem duas raízes reais\033[m")
 elif delta == 0:
-    print("Delta tem uma única raiz real")
+    print("\n\033[33mDelta tem uma única raiz real\033[m")
 else:
-    print("Delta não tem raiz")
+    print("\n\033[30mDelta não tem raiz\033[m")
+
+# Código descobrindo o valor de delta e as raízes caso existam
+# Fórmula do delta: Δ = b**2 – 4 * a * c
+# Fórmula para descobrir as raízes: x = – b ± √Δ / 2 * a
