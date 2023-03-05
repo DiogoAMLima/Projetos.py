e0238219f4cbbf95eaa384e91ea749d39abfcbfd
Diogo de Albuquerque <96998329+DiogoAMLima@users.noreply.github.com>
2023-03-05 17:39:50 -0300

    Documentation added

--- Math/multiplas_variaveis_uma_linha.py
+++ Math/multiplas_variaveis_uma_linha.py
@@ -1,3 +1,4 @@
 nome, nacionalidade, profissao = input('Nome: '), input('Nacionalidade: '), input('Profissão: ')
+# Algumas variáveis declaradas na mesma linha, assim como os inputs
 
 print(f'\033[31m{nome}\033[m, \033[97m{nacionalidade}\033[m, \033[32m{profissao}\033[m')
