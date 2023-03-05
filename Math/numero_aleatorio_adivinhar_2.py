b674681631cee52844321391a82c00af33cccbda
Diogo de Albuquerque <96998329+DiogoAMLima@users.noreply.github.com>
2023-03-05 18:14:19 -0300

    Documentation added and code improved

--- Math/numero_aleatorio_adivinhar_2.py
+++ Math/numero_aleatorio_adivinhar_2.py
@@ -1,23 +1,21 @@
 from random import randint
-print('--------------------------------NÚMERO ALEATÓRIO 2--------------------------------')
 
-print('\n')
+print('\033[35m—\033[m' * 18, '\033[97mNÚMERO ALEATÓRIO\033[m', '\033[35m—\033[m' * 18)
 
 tentativas = 0
-right = False
-while not right:
+right = False  # Variável booleana
+while not right:  # Lopping: enquanto não for False, o looping continua
     aleatorio = randint(1, 10)
-    numero = int(input("Informe o número que você acha que foi sorteado entre 1 e 10... "))
-    if numero == aleatorio:
-        right = True
-        print(f'Você acertou, {numero} = {aleatorio}')
-        tentativas += 1
-        print(f'Você tentou {tentativas} vezes')
+    numero = int(input("\nInforme o número que você acha que foi sorteado entre 1 e 10... "))
+    if numero == aleatorio:  # Verificando se o número escolhido é igual ao número sorteado
+        right = True  # Se a condição acima for verdade, o looping será encerrado
+        print(f'\n\033[36mVocê acertou, número escolhido: {numero} = número sorteado: {aleatorio}\033[m')
+        tentativas += 1  # Incremento do iterador
+        print(f'\nVocê tentou \033[31m{tentativas}\033[m vezes')
     else:
-        print(f'Não foi dessa vez, {numero} != {aleatorio}')
-        tentativas += 1
-        print(f'Você tentou {tentativas} vezes')
+        print(f'\n\033[33mNão foi dessa vez, número escolhido: {numero} != número sorteado: {aleatorio}\033[m')
+        tentativas += 1  # Incremento do iterador
+        print(f'\nVocê tentou \033[37m{tentativas}\033[m vezes')
 
-print('\n')
-
-print('----------------------------------------------------------------------------------')
+print()
+print('\033[35m—\033[m' * 45)  # Multiplicando o texto dentro de '', no caso é uma barra: —
