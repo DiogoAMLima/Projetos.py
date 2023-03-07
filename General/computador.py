0952685925b6a231f01d4a90e48da3cab3479483
Diogo de Albuquerque <96998329+DiogoAMLima@users.noreply.github.com>
2023-03-07 02:09:01 -0300

    Documentation added and code improved

--- General/computador.py
+++ General/computador.py
@@ -1,47 +1,47 @@
-contador_total = 0
-contador_pro_1 = 0
-contador_pro_2 = 0
-contador_pro_3 = 0
-contador_pro_4 = 0
+# Declarando as variáveis na mesma linha:
+contador_total, contador_pro_1, contador_pro_2, contador_pro_3, contador_pro_4 = 0, 0, 0, 0, 0
 
-identificador = int(input("Informe a identificação (diferente de Zero): "))
-while identificador == 0:
-    if identificador == 0:
-        print('Não pode ter identificador igual a 0...')
-    identificador = int(input("Informe a identificação (diferente de Zero): "))
-while identificador != 0:
-    print("1 - Troca de Monitor.")
+while True:  # Lopping
+    # Mostrando menu
+    print("\033[36m1 - Troca de Monitor.")
     print("2 - Troca da Placa-Mãe.")
     print("3 - Troca de fios, teclados ou mouses.")
-    print("4 - Limpeza completa do PC.")
-    defeito = int(input("Informe o número do serviço que deseje: "))
-    if defeito == 1:
-        contador_pro_1 = contador_pro_1 + 1
-    elif defeito == 2:
-        contador_pro_2 = contador_pro_2 + 1
-    elif defeito == 3:
-        contador_pro_3 = contador_pro_3 + 1
-    elif defeito == 4:
-        contador_pro_4 = contador_pro_4 + 1
-    contador_total = contador_total + 1
-    if defeito >= 5:
-        print("Não existe serviço para este número. Tente outra vez...")
-        contador_total = contador_total - 1
-    if defeito <= -1:
-        print("Não existe serviço para este número. Tente outra vez... ")
-        contador_total = contador_total - 1
-    identificador = int(input("Informe a identificação ou tecle 0 para sair: "))
-    try:
-        p1 = contador_pro_1 / contador_total * 100.0
-        p2 = contador_pro_2 / contador_total * 100.0
-        p3 = contador_pro_3 / contador_total * 100.0
-        p4 = contador_pro_4 / contador_total * 100.0
-        if identificador == 0:
-            print("Situação                                   Quantidade    Precentual")
-            print("1 - Troca de Monitor                          {0}          {1:.2f}%".format(contador_pro_1, p1))
-            print("2 - Troca de Placa-Mãe                        {0}          {1:.2f}%".format(contador_pro_2, p2))
-            print("3 - Troca de fios, teclados ou mouses         {0}          {1:.2f}%".format(contador_pro_3, p3))
-            print("4 - Limpeza completa do PC                    {0}          {1:.2f}%".format(contador_pro_4, p4))
-            print("Quantidade de serviços: {0}".format(contador_total))
-    except ZeroDivisionError:
-        print('Não é permitido divisão por 0...')
+    print("4 - Limpeza completa do PC.\033[m")
+    try:  # Tratamento de erros
+        defeito = int(input("\nInforme o número do serviço que deseja: "))
+        if defeito == 1:
+            contador_pro_1 = contador_pro_1 + 1  # Incremento da quantidade
+        elif defeito == 2:
+            contador_pro_2 = contador_pro_2 + 1  # Incremento da quantidade
+        elif defeito == 3:
+            contador_pro_3 = contador_pro_3 + 1  # Incremento da quantidade
+        elif defeito == 4:
+            contador_pro_4 = contador_pro_4 + 1  # Incremento da quantidade
+        contador_total = contador_total + 1
+        if defeito >= 5 or defeito <= -1:  
+            # Se for maior que 5 ou menor igual a -1, o código informa que não existe serviço para estas opções
+            print("\n\033[31mNão existe serviço para este número. Tente outra vez...\033[m")
+            contador_total = contador_total - 1
+        try:
+            # Cáluco da porcentagem
+            p1 = contador_pro_1 / contador_total * 100.0
+            p2 = contador_pro_2 / contador_total * 100.0
+            p3 = contador_pro_3 / contador_total * 100.0
+            p4 = contador_pro_4 / contador_total * 100.0
+            print("\nSituação                         Quantidade(s)    Precentual")
+            print(f"1 - Troca de Monitor                  \033[32m{contador_pro_1}\033[m           \033[34m{p1:.2f}%\033[m")
+            print(f"2 - Troca de Placa-Mãe                \033[32m{contador_pro_2}\033[m           \033[34m{p2:.2f}%\033[m")
+            print(f"3 - Troca de fios, teclados ou mouses \033[32m{contador_pro_3}\033[m           \033[34m{p3:.2f}%\033[m")
+            print(f"4 - Limpeza completa do PC            \033[32m{contador_pro_4}\033[m           \033[34m{p4:.2f}%\033[m")
+            print(f"\nQuantidade de serviços: \033[33m{contador_total}\033[m\n")
+        except ZeroDivisionError:
+            print('\n\033[31mNão é permitido divisão por 0...\033[m')
+    except ValueError:
+        print('\n\033[31mValor inválido!\033[m\n')
+    esc = str(input('Deseja continuar? [S/N] ')).upper()
+    if esc in 'N':
+        print('\n\033[32mAté a próxima!\033[m')
+        break
+    else:
+        print()
+        pass
