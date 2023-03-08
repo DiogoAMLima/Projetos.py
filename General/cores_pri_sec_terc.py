09a2672335b56aabd55449b610de9a473a8d5b4b
Diogo de Albuquerque <96998329+DiogoAMLima@users.noreply.github.com>
2023-03-08 00:56:50 -0300

    Documentation added and code improved

--- General/cores_pri_sec_terc.py
+++ General/cores_pri_sec_terc.py
@@ -1,59 +1,51 @@
 from random import randint
 
-print('---------------------------------------------------CORES---------------------------------------------------\n')
+print('—' * 18, '\033[35mCORES\033[m', '—' * 18, '\n')
 
-primarias = ('Vermelho', 'Azul', 'Amarelo')
-aleatorio = randint(0, 2)
+primarias = ('\033[31mVermelho\033[m', '\033[34mAzul\033[m', '\033[33mAmarelo\033[m')
+aleatorio = randint(0, 2)  # Número aleatório entre 0 e 2
 print("Escolha uma cor:\n"
       "[0] = Vermelho\n"
       "[1] = Azul\n"
       "[2] = Amarelo\n")
 escolha = int(input("Qual a sua escolha? "))
 
-print('\n')
+print(f"\nCor escolhida aleatoriamente {primarias[aleatorio]}")
 
-print("Cor escolhida aleatoriamente {}".format(primarias[aleatorio]))
-
-print("\n")
-
-print("Sua escolha foi {}".format(primarias[escolha]))
-
-print("\n")
+print(f"\nSua escolha foi {primarias[escolha]}")
 
 if aleatorio == 0:  # Vermelho
     if escolha == 0:
-        print("Teremos Vermelho (Cor Primária)")
+        print("\nTeremos Vermelho (Cor Primária)")
     elif escolha == 1:
-        print("Teremos Roxo (Cor Secundária)")
-        print("Agora com o Roxo, podemos misturar com o Vermelho e teremos uma cor Terciária: Vermelho-Arroxeado!")
+        print("\nTeremos Roxo (Cor Secundária)")
+        print("\nAgora com o Roxo, podemos misturar com o Vermelho e teremos uma cor Terciária: Vermelho-Arroxeado!\n")
     elif escolha == 2:
-        print("Teremos Laranja (Cor Secundária)")
-        print("Agora com o Laranja, podemos misturar com o Vermelho e teremos uma cor Terciária: Vermelho-Alaranjado!")
+        print("\nTeremos Laranja (Cor Secundária)")
+        print("\nAgora com o Laranja, podemos misturar com o Vermelho e teremos uma cor Terciária: Vermelho-Alaranjado!\n")
     else:
-        print("Opção inválida")
+        print("\n\033[31mOpção inválida\033[m")
 elif aleatorio == 1:  # Azul
     if escolha == 0:
-        print("Teremos Roxo (Cor Secundária)")
-        print("Agora com o Roxo, podemos misturar com o Azul e teremos uma cor Terciária: Azul-Arroxeado!")
+        print("\nTeremos Roxo (Cor Secundária)")
+        print("\nAgora com o Roxo, podemos misturar com o Azul e teremos uma cor Terciária: Azul-Arroxeado!\n")
     elif escolha == 1:
-        print("Teremos Azul (Cor Primária)")
+        print("\nTeremos Azul (Cor Primária)")
     elif escolha == 2:
-        print("Teremos Verde (Cor Secundária)")
-        print("Agora com o Verde, podemos misturar com o Azul e teremos uma cor Terciária: Azul-Esverdeado!")
+        print("\nTeremos Verde (Cor Secundária)")
+        print("\nAgora com o Verde, podemos misturar com o Azul e teremos uma cor Terciária: Azul-Esverdeado!\n")
     else:
-        print("Opção inválida")
+        print("\n\033[31mOpção inválida\033[m")
 elif aleatorio == 2:  # Amarelo
     if escolha == 0:
-        print("Teremos Laranja (Cor Secundária)")
-        print("Agora com o Laranja, podemos misturar com o Amarelo e teremos uma cor Terciária: Amarelo-Alaranjado!")
+        print("\nTeremos Laranja (Cor Secundária)")
+        print("\nAgora com o Laranja, podemos misturar com o Amarelo e teremos uma cor Terciária: Amarelo-Alaranjado!\n")
     elif escolha == 1:
-        print("Teremos Verde (Cor Secundária)")
-        print("Agora com o Verde, podemos misturar com o Amarelo e teremos uma cor Terciária: Amarelo-Esverdeado!")
+        print("\nTeremos Verde (Cor Secundária)")
+        print("\nAgora com o Verde, podemos misturar com o Amarelo e teremos uma cor Terciária: Amarelo-Esverdeado!\n")
     elif escolha == 2:
-        print("Teremos Amarelo (Cor Primária)")
+        print("\nTeremos Amarelo (Cor Primária)")
     else:
-        print("Opção inválida")
+        print("\n\033[31mOpção inválida\033[m")
 
-print('\n')
-
-print('-------------------------------------------------------------------------------------------------------------')
+print('\033[36m—\033[m' * 80)
