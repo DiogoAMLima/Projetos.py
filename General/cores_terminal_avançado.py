97e9ee2860d481b281d644bf98846836b82284cb
Diogo de Albuquerque <96998329+DiogoAMLima@users.noreply.github.com>
2023-03-08 01:38:21 -0300

    Documentation added and code improved

--- General/cores_terminal_avançado.py
+++ General/cores_terminal_avançado.py
@@ -1,10 +1,10 @@
-print('------------------------------------------CORES TERMINAL AVANÇADO------------------------------------------\n')
+print('—' * 18, '\033[31mCORES TERMINAL AVANÇADO\033[m', '—' * 18, '\n')
 videogame = 'Playstation 4'
 valor = float(input('Informe o valor do Playstation 4: '))
 opcoes = {'finalizar': '\033[m', 'Roxo': '\033[35m', 'Vermelho': '\033[31m', 'Cinza': '\033[37m', 'Branco': '\033[30m'}
-print("O {}{}{} está em Cinza e temos o seu valor em Vermelho: {}{:.2f}{}"
-      .format(opcoes['Cinza'], videogame, opcoes['finalizar'],
-              opcoes['Vermelho'], valor, opcoes['finalizar']))
-print('\n')
+# Lista de opções acima
+# Finalizar recebe apenas a última parte do código de cores
+print(f"\nO {opcoes['Cinza']}{videogame}{opcoes['finalizar']} está em Cinza e temos o seu valor em Vermelho: "
+      f"{opcoes['Vermelho']}{valor}{opcoes['finalizar']}\n")
 
-print('-------------------------------------------------------------------------------------------------------------')
+print('\033[33m—\033[m' * 80)
