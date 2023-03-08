c8c4773a980819e2155ecccfe7fade0eac920107
Diogo de Albuquerque <96998329+DiogoAMLima@users.noreply.github.com>
2023-03-08 02:00:00 -0300

    Documentation added and code improved

--- General/cp.py
+++ General/cp.py
@@ -1,63 +1,51 @@
-(input("Aperte alguma tecla para as perguntas aparecerem... "))
+(input("\n\033[35mAperte alguma tecla para as perguntas aparecerem...\033[m "))
+
+# Declarando as variáveis
 
 pergunta = 1
-resposta = ''
-resposta_2 = ''
-resposta_3 = ''
-resposta_4 = ''
 
-valor1 = 'Luffy, Zoro, Nami, Usopp, Sanji, Chopper, Robin, Franky, Brook, Jinbe'
-valor2 = 272
-valor3 = 663
-valor4 = 131
-
-
-while pergunta != 0:
-    print("1 - Informe a ordem de entrada dos tripulantes do chapéu de palha!\n")
+while pergunta != 0:  # Looping, só termina quando o usuário digitar zero
+    global resposta_2, resposta_3, resposta_4  # Inicializando as variáveis como globais para usar 
+    # dentro do looping sem precisar declarar fora do looping com algum valor
+    print("\n\033[33m1 - Informe a ordem de entrada dos tripulantes do chapéu de palha!\n")
     print("2 - Número do episódio da primeira aparição do Gear 1?\n")
     print("3 - Em que episódio Sabo retorna?\n")
-    print("4 - Número do episódio da primeira aparição do Barba negra?\n")
-    pergunta = int(input("Informe o número da pergunta que quer responder ou digite 0 para sair: "))
-    if pergunta == 0:
-        print("Até a próxima...")
-    if pergunta == 1:
-        print("1 - Informe a ordem de entrada dos tripulantes do chapéu de palha!")
-        resposta: str = str(input())
-        if resposta == valor1:
-            print("Resposta certa!\n")
-        if resposta != valor1:
-            print("Resposta errada!\n")
-    if pergunta == 2:
-        print("2 - Número do episódio da primeira aparição do Gear 1?")
-        try:
-            resposta_2: int = int(input())
+    print("4 - Número do episódio da primeira aparição do Barba negra?\033[m")
+    pergunta = int(input("\nInforme o número da pergunta que quer responder ou digite 0 para sair: "))
+    if pergunta == 0:  # Condicional
+        print("\n\033[32mAté a próxima...\033[m")
+    elif pergunta == 1:  # Condicional
+        resposta = str(input("\n1 - Informe a ordem de entrada dos tripulantes do chapéu de palha: "))
+        if resposta == 'Luffy, Zoro, Nami, Usopp, Sanji, Chopper, Robin, Franky, Brook, Jinbe':  # Condicional
+            print("\n\033[34mResposta certa!\033[m")
+        elif resposta != 'Luffy, Zoro, Nami, Usopp, Sanji, Chopper, Robin, Franky, Brook, Jinbe':  # Condicional
+            print("\n\033[31mResposta errada!\033[m")
+    elif pergunta == 2:  # Condicional
+        try:  # Tratamento de erros
+            resposta_2 = int(input("\n2 - Número do episódio da primeira aparição do Gear 1? "))
         except ValueError:
-            print('Você digitou um número real, digite um número inteiro...')
-        if resposta_2 == valor2:
-            print("Resposta certa!\n")
-        if resposta_2 != valor2:
-            print("Resposta errada!\n")
-    if pergunta == 3:
-        print("3 - Em que episódio Sabo retorna?")
-        try:
-            resposta_3: int = int(input())
+            print('\n\033[36mVocê digitou um número real, digite um número inteiro...\033[m')
+        if resposta_2 == 272:  # Condicional
+            print("\n\033[34mResposta certa!\033[m")
+        elif resposta_2 != 272:  # Condicional
+            print("\n\033[31mResposta errada!\033[m")
+    elif pergunta == 3:  # Condicional
+        try:  # Tratamento de erros
+            resposta_3 = int(input("\n3 - Em que episódio Sabo retorna? "))
         except ValueError:
-            print('Você digitou um número real, digite um número inteiro...')
-        if resposta_3 == valor3:
-            print("Resposta certa!\n")
-        if resposta_3 != valor3:
-            print("Resposta errada!\n")
-    if pergunta == 4:
-        print("4 - Número do episódio da primeira aparição do Barba negra?")
-        try:
-            resposta_4: int = int(input())
+            print('\n\033[36mVocê digitou um número real, digite um número inteiro...\033[m')
+        if resposta_3 == 663:  # Condicional
+            print("\n\033[34mResposta certa!\033[m")
+        elif resposta_3 != 663:  # Condicional
+            print("\n\033[31mResposta errada!\033[m")
+    elif pergunta == 4:  # Condicional
+        try:  # Tratamento de erros
+            resposta_4 = int(input("\n4 - Número do episódio da primeira aparição do Barba negra? "))
         except ValueError:
-            print('Você digitou um número real, digite um número inteiro...')
-        if resposta_4 == valor4:
-            print("Resposta certa!\n")
-        if resposta_4 != valor4:
-            print("Resposta errada!\n")
-    elif pergunta >= 5:
-        print("Número incorreto, tente outra vez...")
-    elif pergunta <= -1:
-        print("Número incorreto, tente outra vez...")
+            print('\n\033[36mVocê digitou um número real, digite um número inteiro...\033[m')
+        if resposta_4 == 131:  # Condicional
+            print("\n\033[34mResposta certa!\033[m")
+        elif resposta_4 != 131:  # Condicional
+            print("\n\033[31mResposta errada!\033[m")
+    elif pergunta >= 5 or pergunta <= -1:
+        print("\n\033[31mNúmero incorreto, tente outra vez...\033[m")
