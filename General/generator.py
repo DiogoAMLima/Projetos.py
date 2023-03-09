80f577f2baf8a17995432157f8500b54deb55cce
Diogo de Albuquerque <96998329+DiogoAMLima@users.noreply.github.com>
2023-03-08 23:40:36 -0300

    Documentation added and code improved

--- General/generator.py
+++ General/generator.py
@@ -2,6 +2,7 @@
 
 works = [("trabalho", 70), ("trabalho", 60), ("trabalho", 100)]
 work_time = (work[1] for work in works if work[0] == "trabalho")
+# Para cada variável dentro de works se o a posição da primeira variável for == trabalho
 # print(sum(work_time))  # Podemos fazer sem colocar mais uma variável
-worked_time = sum(work_time)
+worked_time = sum(work_time)  # Somando tudo
 print(f'\n\033[36mTempo trabalhado foi de: {worked_time}\033[m')
