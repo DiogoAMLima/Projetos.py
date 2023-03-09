ae30819b32cc66368bf9366c20ba06b68747626b
Diogo de Albuquerque <96998329+DiogoAMLima@users.noreply.github.com>
2023-03-09 00:23:25 -0300

    Documentation added

--- General/progresso_2.py
+++ General/progresso_2.py
@@ -4,8 +4,10 @@ from tqdm import tqdm
 x = int(input('Informe a quantidade de aulas assistidas: '))
 y = 343
 i = 0
-for i in tqdm(range(y)):
-    time.sleep(0.1)
-    if i == x:
+for i in tqdm(range(y)):  # Para cada número no alcance do método tqdm com o parâmetro y passado
+    time.sleep(0.1)  # Tempo de pausa para a próxima ação
+    if i == x:  # Se o iterador for igual ao número escolhido, o looping acaba
         break
-print(f'\n\n\033[33m{i}\033[m aulas assisitdas, seu progresso é de: \033[34m{(x/y) * 100:.2f}%\033[m')
+print(f'\n\n\033[33m{i}\033[m aulas assisitdas, seu progresso é de: \033[34m{(x/y) * 100:.2f}%\033[m')  # Porcentagem
+
+# Esta barra de progresso mostra o número atual até o número final 343 ao lado da barra sendo carregada
