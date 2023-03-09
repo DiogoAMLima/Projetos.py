bd86c2e37c0f2699f961f9d8828abc102121d830
Diogo de Albuquerque <96998329+DiogoAMLima@users.noreply.github.com>
2023-03-08 23:54:21 -0300

    Documentation added and code improved

--- General/gerador_senha_5_cracker.py
+++ General/gerador_senha_5_cracker.py
@@ -1,20 +1,23 @@
 import random
 
+# Escolhendo todos os caracteres
+
 all_character = r"0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%&*/\?:"
-character_list = list(all_character)
+character_list = list(all_character)  # Colocando em uma lista
 
 password = input("Digite sua senha: ")
 
 cracker = ''
 
-i = 0
+i = 0  # Iterador
 
-while cracker != password:
-    cracker = random.choices(character_list, k=len(password))
+while cracker != password:  # Enquanto o cracker for diferente da senha escolhida o looping continua
+    cracker = random.choices(character_list, k=len(password))  # Retorna uma lista de caracteres selecionados de
+    # acordo com o tamanho passado a constante k, que é o tamanho da nossa lista
     print(f"\033[31m-=\033[m" * 10 + f"\033[35m{str(cracker)}\033[m" + "\033[31m-=\033[m" * 10)
-    i += 1
-    if cracker == list(password):
-        print("Sua senha é: " + "".join(cracker))
+    i += 1  # Iterador para descobrirmos no final, quantas vezes tentamos "quebrar a senha"
+    if cracker == list(password):  # Se o cracker for igual a lista ao password (transformado em lista)
+        print("\nSua senha é: " + "".join(cracker))  # Juntando o cracker com sua senha é:
         break
 
-print(f'Demorou \033[34m{i}\033[m tentativas para descobrir a sua senha!')
+print(f'\nDemorou \033[34m{i}\033[m tentativas para descobrir a sua senha!')
