30122c9c7710cd58520557af20a4d7ebbc115811
Diogo de Albuquerque <96998329+DiogoAMLima@users.noreply.github.com>
2023-03-09 01:03:27 -0300

    Documentation added and code improved

--- General/zip.py
+++ General/zip.py
@@ -18,17 +18,17 @@ z = []
 
 valores = int(input('Informe a quantidade de valores que terá nas lista: '))
 
-i = 0
+i = 0  # Iterador
 
-for i in range(valores):
+for i in range(valores):  # Looping até o tamanho de valores
     values = int(input('\nInforme o valor a ser adicionado na lista x: '))
-    x.append(values)
+    x.append(values)  # Adicionando na lista
     values2 = int(input('Informe o valor a ser adicionado na lista y: '))
-    y.append(values2)
+    y.append(values2)  # Adicionando na lista
     values3 = int(input('Informe o valor a ser adicionado na lista z: '))
-    z.append(values3)
+    z.append(values3)  # Adicionando na lista
 
 for a, b, c in zip(x, y, z):
-    print(f'\n\033[31m{a}\033[m, \033[33m{b}\033[m, \033[34m{c}\033[m')
+    print(f'\n\033[31m1° valor: {a}\033[m, \033[33m2° valor: {b}\033[m, \033[34m3° valor: {c}\033[m')
 
 # Não é necessário o parâmetro strict porque já definimos o tamanho da lista anteriormente para todas as listas
