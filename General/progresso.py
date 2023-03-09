1fdcb5953d0ef7affe012faede5a25203d0c4ddf
Diogo de Albuquerque <96998329+DiogoAMLima@users.noreply.github.com>
2023-03-09 00:19:49 -0300

    Documentation added

--- General/progresso.py
+++ General/progresso.py
@@ -2,14 +2,14 @@ import pyprind
 import time
 
 x = int(input('Informe a quantidade de aulas assistidas: '))
-y = 343
+y = 318
 i = 0
-bar = pyprind.ProgBar(y)
+bar = pyprind.ProgBar(y)  # Barra recebe o método pyprind.ProgBar e colocamos a variável como parâmetro
 print()
-while i != y:
-    time.sleep(0.1)
+while i != y:  # Se o iterador for diferente do número determinado anteriormente
+    time.sleep(0.1)  # Tempo de pausa para a próxima ação
     i += 1
-    if i == x:
+    if i == x:  # Se o iterador for igual ao número de aulas escolhido, ele faz um update na barra de progresso e para
         break
-    bar.update()
-print(f'\n\n\033[33m{i}\033[m aulas assisitdas, seu progresso é de: \033[34m{(x/y) * 100:.2f}%\033[m')
+    bar.update()  # Fazendo o update da barra de progresso
+print(f'\n\n\033[33m{i}\033[m aulas assisitdas, seu progresso é de: \033[34m{(x/y) * 100:.2f}%\033[m')  # Porcentagem
