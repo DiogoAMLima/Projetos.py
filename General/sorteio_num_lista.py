d05e767745a14ed983ef5ba80e1450f888e7f354
Diogo de Albuquerque <96998329+DiogoAMLima@users.noreply.github.com>
2023-03-09 00:33:49 -0300

    Documentation added

--- General/sorteio_num_lista.py
+++ General/sorteio_num_lista.py
@@ -1,14 +1,14 @@
 import random
 
-lista = []
+lista = []  # Declarando a lista
 
 alcance = int(input('Informe quantos números terão na lista: '))
 print()
 
-for i in range(0, alcance):
+for i in range(0, alcance):  # Looping de 0 até o número determinado
     num = float(input(f'Informe um número na posição {i}: '))
-    lista.append(num)
+    lista.append(num)  # Adicionando o número a lista
 
-print(f'\nNúmeros escolhidos: \033[31m{lista}\033[m')
+print(f'\nNúmeros escolhidos: \033[31m{lista}\033[m')  # Lista final
 
-print(f'\nNúmero sorteado: \033[36m{random.choice(lista)}\033[m')
+print(f'\nNúmero sorteado: \033[36m{random.choice(lista)}\033[m')  # Sorteando um número da lista final
