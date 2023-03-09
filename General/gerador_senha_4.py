ebd2dd1d382259c14d10a5a94b100d3c6032bc5a
Diogo de Albuquerque <96998329+DiogoAMLima@users.noreply.github.com>
2023-03-08 23:48:47 -0300

    Documentation added and code improved

--- General/gerador_senha_4.py
+++ General/gerador_senha_4.py
@@ -3,9 +3,11 @@ import string
 import time
 
 qntd = int(input('Informe quantos caracteres terá a senha: '))
-
+# Usando a bilioteca string, não precisamos usar variáveis como no código de gerador de senha 
 password = "".join(random.choices(string.printable, k=qntd)).replace(" ", ".")
-
+# Unindo os caracteres com .join gerados pelo método choices de acordo com o string.printable que tem os caracteres
+# de acordo com a variável k que determinará o tamanho da senha
+    
 print('\n\033[31mEstamos gerando sua senha, por favor aguarde!!!\033[m')
 time.sleep(1.5)
 
