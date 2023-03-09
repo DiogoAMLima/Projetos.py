371d3e52a0242099f63e06f9869689be7dc5b64c
Diogo de Albuquerque <96998329+DiogoAMLima@users.noreply.github.com>
2023-03-09 00:44:47 -0300

    Documentation added and code improved

--- General/tuplas_2.py
+++ General/tuplas_2.py
@@ -3,6 +3,7 @@
 lista_palavras = ['Álbum', 'Crachá', 'Piauí', 'Grajaú', 'Sótão', 'Jacarés', 'Tórax', 'Célebre']
 
 opcoes = str(input('Sem ideias para testar? Deseja ver uma lista de sugestões de palavras? [S/N]? ')).strip().upper()
+# Excluindo espaços vazios com o .strip() e aumentando a letra com o .upper()
 if opcoes in 'S':
     print(f'\033[97m{lista_palavras}\033[m')
 else:
@@ -14,8 +15,8 @@ palavra_3 = str(input('Informe mais uma palavra: '))
 
 palavras = (f'{palavra_1}', f'{palavra_2}', f'{palavra_3}')
 
-for palavra in palavras:
-    print(f'\nNa palavra \033[31m{palavra.upper()}\033[m temos', end=' ')
+for palavra in palavras:  # Verificando cada palavra das 3 palavras escolhidas
+    print(f'\nNa palavra \033[31m{palavra.upper()}\033[m temos as vogais', end=' ')  # Mostrando os nomes
     for letra in palavra:
-        if letra.lower() in 'aáãâeéêiíoóuú':
-            print(f'\033[36m{letra}\033[m', end=' ')
+        if letra.lower() in 'aáãâeéêiíoóuú':  # Verificando se é vogal
+            print(f'\033[36m{letra}\033[m', end=' ')  # Mostrando as vogais
