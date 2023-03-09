4a6031f15724d3c3fe1700fc24df502f6c4cd712
Diogo de Albuquerque <96998329+DiogoAMLima@users.noreply.github.com>
2023-03-09 00:15:19 -0300

    Documentation added

--- General/lista_uma_linha.py
+++ General/lista_uma_linha.py
@@ -1,3 +1,6 @@
 lista = [linha.strip() for linha in open("lista.txt", "r")]
+# Separando cada linha que estiver dentro do arquivo lista.txt
+# "r" é de read, para ler o arquivo
 
 print(f'\033[31m{lista}\033[m')
+
